0c3187090fd03403afea202158eb0b07214ec418
Brian Min <bymi15@yahoo.com>
2023-02-24 00:53:00 +0000

    comment out terms from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ export default function Footer() {
       <div className="max-w-5xl xl:max-w-5xl mx-auto divide-y px-4 sm:px-6 md:px-8">
         <div className="flex flex-col justify-between pt-5 pb-4 sm:flex-row border-none">
           <ul className="flex flex-col space-y-2 sm:mb-0 items-center sm:space-y-0 sm:space-x-5 sm:flex-row">
-            <li>
+            {/* <li>
               <Link
                 href="/terms"
                 className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
               >
                 Terms of Service
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link
                 href="/privacy"
