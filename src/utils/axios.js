493d52ee24a34ff121b0f766bc91f46153c1daf9
Brian Min <bymi15@yahoo.com>
2023-02-27 18:48:33 +0000

    increase axios timeout

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 // axios instance for public api calls
 export const axiosPublicInstance = axios.create({
-  timeout: 10000,
+  timeout: 30000,
 });
 
 // axios server instance for NextJS API (Proxy) to call Flow NFT Alert API
 export const axiosServerInstance = axios.create({
   baseURL: process.env.API_URL,
-  timeout: 10000,
+  timeout: 30000,
 });
 
 export const setServerAuthHeader = (req) =>
